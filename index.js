8a4e88831e0bb3ebede8cd646733f1ebb4fad8c6
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-05-16 00:18:10 +0600

    mongo connection done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
@@ -7,6 +8,20 @@ const port = process.env.PORT || 5000;
 // Using the middlewares
 app.use(cors());
 app.use(express.json());
+//connecting the mongo db
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2kgz6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("MOngo db connected");
+  client.close();
+});
 
 // The main api
 app.get("/", (req, res) => {
